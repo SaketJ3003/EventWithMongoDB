49d7bad11fed9697e98b5bc1da428075f8311c2b
SaketJ3003 <saketjangir3003@gmail.com>
2026-06-20 17:41:30 +0530

    Fix: add WHITENOISE_USE_FINDERS=True so CSS loads in dev without collectstatic

--- eventProject/eventProject/settings.py
+++ eventProject/eventProject/settings.py
@@ -201,6 +201,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# WhiteNoise: also serve from STATICFILES_DIRS in DEBUG mode (no collectstatic needed locally)
+WHITENOISE_USE_FINDERS = True
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
